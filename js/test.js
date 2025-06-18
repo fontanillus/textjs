924e116bf4bb85c481c449696781a056a4b4c161
fontanillus <arcamapa@gmail.com>
2025-06-18 10:48:06 +0200

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,52 @@
+const tema = 'Test de repaso';
+
+const preguntas = [
+    {
+        numpregunta: "1",
+        pregunta: "¿Cuál es el código HTML correcto para un salto de línea?",
+        opcionA: "&lt;br&gt",
+        opcionB: "&lt;break&gt",
+        opcionC: "&lt;p&gt", // &lt; < y &gt >
+        opcionD: "&lt;hr&gt",
+        correcta: "A",
+        respuesta: "",
+        puntos: 1
+        // img: cuando la pregunta contriene una imagen
+    },
+    {
+        numpregunta: "2",
+        pregunta: "¿Cómo podemos definir el concepto clase dentro de la programación orientada a objetos?",
+        opcionA: "Una entidad general que contiene todas las características de los objetos y de las que derivan estos",
+        opcionB: "Una entidad concreta que contiene todas las características de los objetos y de las que derivan estos",
+        opcionC: "Un objeto general de las que derivan otras clases",
+        opcionD: "Un objeto típico de la programación en general no solo orientada a objetos",
+        correcta: "A",
+        respuesta: "",
+        puntos: 1
+    },
+
+    {
+        numpregunta: "3",
+        pregunta: "¿A que se puede asemejar el concepto de método en la programación orientada a objetos?",
+        opcionA: "Al concepto de función",
+        opcionB: "Al concepto de objetos",
+        opcionC: "Al concepto de clase",
+        opcionD: "Al concepto de programa",
+        correcta: "A",
+        respuesta: "",
+        puntos: 1
+    },
+    {
+        numpregunta: "4",
+        pregunta: "¿Que es un lenguaje orientado a eventos?",
+        opcionA: "Es aquel que responde cuando se produce una accion, ya sea producida por el usuario o bien por el proceso normal de la página",
+        opcionB: "Es aquel que produce los eventos mediante su código",
+        opcionC: "Es aquel en el que su código está formado por eventos",
+        opcionD: "No existen esos lenguajes, lo correcto es llamarlos orientados a objetos",
+        correcta: "A",
+        respuesta: "",
+        puntos: 1
+    },
+
+];
+
